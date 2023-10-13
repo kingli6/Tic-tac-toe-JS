9824fec8c79ac26825be612e4bf824b0df2c1ec2
Joanthan Fisher <jonathan.fshr@gmail.com>
2023-10-13 17:32:17 +0200

    board function. building cirkle

--- app.js
+++ app.js
@@ -3,12 +3,20 @@ const infoDisplay = document.querySelector('#info');
 const startCells = ['', '', '', '', '', '', '', '', ''];
 
 function createBoard() {
-  // adding _ to a variable means we won't be using it...
+  // !! adding _ to cell ("_cell") variable means we won't be using it...
   startCells.forEach((_cell, index) => {
     const cellElement = document.createElement('div');
     cellElement.classList.add('square');
+    // .classList adds a class to the element: class="square"
+    cellElement.id = index;
+    // .id adds an id to the element: id="0"
+    cellElement.addEventListener('click', addGo);
     gameBoard.append(cellElement);
   });
 }
 
 createBoard();
+
+function addGo(e) {
+  console.log(e.target);
+}
