42908a4c87092cd21fc94814352691857a987954
Joanthan Fisher <jonathan.fshr@gmail.com>
2023-10-27 15:57:09 +0200

    type conversion

--- pages/script.js
+++ pages/script.js
@@ -1,3 +1,29 @@
+// 06-type-conversion
+let amount = 'hello';
+
+// Convert string to number
+amount = parseInt(amount);
+amount = +amount;
+amount = Number(amount);
+
+// Convert number to string
+amount = amount.toString();
+amount = String(amount);
+
+// Convert string to decimal
+amount = parseFloat(amount);
+
+// Convert number to boolean
+amount = Boolean(amount);
+
+// Ways to get NaN
+console.log(Math.sqrt(-1));
+console.log(1 + NaN);
+console.log(undefined + undefined);
+console.log('foo' / 3);
+
+console.log(amount, typeof amount);
+
 //04-data-types-------------------------------------------------
 //https://github.com/bradtraversy/javascript-sandbox/blob/main/01-variables-data-types/04-data-types/script.js
 // String
